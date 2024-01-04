fab6baffb10256ae463369bb524358e9d8791d2c
Freddy <fescobar@thoughtworks.com>
2024-01-04 16:45:55 -0500

    Fixing local deployment script

--- deploy/deploy_image.sh
+++ deploy/deploy_image.sh
@@ -2,7 +2,8 @@
 
 TAG=todo_app
 
-docker build -t "$TAG" -f 'deploy/prod.Dockerfile' .
+docker build -t "$TAG" -f 'deploy/prod.DockerFile' .
 
 docker run --name "$TAG" -p 8080:80 -d "$TAG"
 #   -e .env \
+
